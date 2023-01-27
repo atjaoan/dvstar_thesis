9febcea4ef63e316d78a679049041b3a3e9d83cf
SebastianHolm <holmse@student.chalmers.se>
2023-01-27 11:04:32 +0100

    Added functionality for deciding which distance function to use in benchmarking + what parameters to use when building VLMCs

--- bench.py
+++ bench.py
@@ -2,10 +2,10 @@ import typer
 import subprocess
 from pathlib import Path
 from typing import Final
-import time
 import re
 import pandas as pd 
 import os 
+from enum import Enum
 
 app = typer.Typer()
 
@@ -13,7 +13,20 @@ K: Final[int] = 9
 
 cwd = Path(__file__).parent
 
-def dvstar_build():
+class Distance_Function(str, Enum):
+    d2 = "d2"
+    d2star = "d2star"
+    dvstar = "dvstar"
+    nearest_dvstar = "nearest-dvstar"
+    penalized_dvstar = "penalized-dvstar"
+    kl = "kl"
+    kl_both = "kl-both"
+    nll = "nll"
+    nll_background = "nll-background"
+    cv = "cv"
+    cv_estimation = "cv-estimation"
+
+def dvstar_build(threshold: float, min_count: int, max_depth: int):
     args = (
         "find",
         "./data/sequences_split_files",
@@ -23,10 +36,12 @@ def dvstar_build():
         "./build/dvstar",
         "--mode",
         "build",
+        "--threshold",
+        str(threshold),
         "--min-count",
-        "10",
+        str(min_count),
         "--max-depth",
-        "9",
+        str(max_depth),
         "--fasta-path",
         "{}",
         "--out-path",
@@ -35,33 +50,16 @@ def dvstar_build():
     )
     subprocess.run(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-def dvstar_cmp(path_1: Path, path_2: Path) -> subprocess.CompletedProcess:
+def calculate_distances(dist_func: str) -> subprocess.CompletedProcess:
     args = (
         "perf",
         "stat",
         "-e branch-misses,branches,task-clock,cycles,instructions,cache-references,cache-misses",
-        cwd / "build/dvstar",
-        "--mode",
-        "5",
-        "--in-path",
-        path_1,
-        "--to-path",
-        path_2
-    )
-    res = subprocess.run(args, capture_output=True, text=True)
-
-    return res
-
-def calculate_distances() -> subprocess.CompletedProcess:
-    args = (
-        "perf",
-        "stat",
-        "-e branch-misses,branches,task-clock,cycles,instructions,cache-references,cache-misses",
-        cwd / "submodules/PstClassifierSeqan/build/src/calculate-distances", # Hard coded path to calculate-distances might need to be changed
+        cwd / "submodules/PstClassifierSeqan/build/src/calculate-distances", 
         "-p",
         cwd / "data/small_test/",
         "-n",
-        "d2"
+        dist_func 
     )
     res = subprocess.run(args, capture_output=True, text=True)
 
@@ -72,7 +70,7 @@ def calculate_distances_only_oh() -> subprocess.CompletedProcess:
         "perf",
         "stat",
         "-e branch-misses,branches,task-clock,cycles,instructions,cache-references,cache-misses",
-        cwd / "submodules/PstClassifierSeqan/build/src/calculate-distances-oh", # Hard coded path to calculate-distances might need to be changed
+        cwd / "submodules/PstClassifierSeqan/build/src/calculate-distances-oh", 
         "-p",
         cwd / "data/small_test/",
         "-n",
@@ -82,13 +80,13 @@ def calculate_distances_only_oh() -> subprocess.CompletedProcess:
 
     return res
 
-def save_to_csv(res: subprocess.CompletedProcess, csv_path: Path):
+def save_to_csv(res: subprocess.CompletedProcess, csv_path: Path, dist_func: str):
     new_line_separated_attr = res.stderr.split('\n')[3:-8]
 
     print(res.stderr)
 
-    data = [get_git_commit_version()]
-    columns = ["Repo Version"]
+    data = [get_git_commit_version(), dist_func]
+    columns = ["Repo Version", "distance_function"]
     
     for line in new_line_separated_attr:
         split_line = line.split('#')
@@ -157,18 +155,18 @@ def dvstar_cmp_mem():
     return
 
 @app.command()
-def stat():
-    timing_results = calculate_distances()
+def stat(dist_func: Distance_Function = Distance_Function.dvstar):
+    timing_results = calculate_distances(dist_func.value)
 
-    save_to_csv(timing_results, cwd / "tmp/benchmarks/test.csv")
+    save_to_csv(timing_results, cwd / "tmp/benchmarks/test.csv", dist_func)
 
 @app.command()
 def record():
     dvstar_cmp_mem()
 
 @app.command()
-def build():
-    dvstar_build()
+def build(threshold: float = 3.9075, min_count: int = 10, max_depth: int = 9):
+    dvstar_build(threshold, min_count, max_depth)
 
 if __name__ == "__main__":
     app()
