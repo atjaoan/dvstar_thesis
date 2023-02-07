c30332bbc0a4a8c2f9df105b82bf585feb26ba3b
SebastianHolm <holmse@student.chalmers.se>
2023-02-07 16:43:46 +0100

    Added template for Cluster container (Class containing many VLMCs) and made the 'get_trees' function handle different container classes

--- include/optimize_cmp_kmers/get_trees.hpp
+++ include/optimize_cmp_kmers/get_trees.hpp
@@ -2,49 +2,18 @@
 
 #include <filesystem>
 
-#include "vlmc_from_kmers/context_archive.hpp"
-#include "vlmc_from_kmers/kmer_container.hpp"
-#include "vlmc_from_kmers/similarity_pruning.hpp"
-#include "vlmc_from_kmers/kmer.hpp"
-#include "optimize_cmp_kmers/vlmc_template.hpp"
-
-constexpr int max_k = 255;
-
 using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
-
-using vlmc_t = container::VLMC_template; 
-//KmerContainer<class Comparator = ReverseKMerComparator<max_k>>;
 
 namespace get_trees{
   
-bool has_next(std::ifstream &file_stream) { return file_stream.peek() != EOF; }
-
-vlmc::VLMCKmer &next(cereal::BinaryInputArchive &iarchive, vlmc::VLMCKmer &kmer) {
-  iarchive(kmer);
-  return kmer;
-}
-
-std::vector<vlmc_t> get_trees(const std::filesystem::path &directory){
-  // vlmc::HashMapKmerContainer kmer_con(); 
-  std::vector<vlmc_t> trees{};
+template<typename CC, typename VC>
+void get_trees(const std::filesystem::path &directory, CC trees){
 
   for (const auto& dir_entry : recursive_directory_iterator(directory)) {
-    std::ifstream ifs(dir_entry.path(), std::ios::binary);
-    cereal::BinaryInputArchive archive(ifs);
-
-    vlmc_t tree{};
-    vlmc::VLMCKmer kmer{};
-
-    while (has_next(ifs)){
-      next(archive, kmer);
-      tree.push(kmer);
-    }
-    trees.push_back(tree);
-    ifs.close();
-  } 
+    VC tree(dir_entry.path());
+    trees.push(tree);
+  }
 
   std::cout << trees.size() << std::endl; 
-  
-  return trees; 
 }
 }
--- src/calc_dists.cpp
+++ src/calc_dists.cpp
@@ -1,8 +1,18 @@
+// #include <Eigen/Dense>
+
 #include "optimize_cmp_kmers/parser.hpp"
 
 #include "optimize_cmp_kmers/distances/dvstar.hpp"
 #include "optimize_cmp_kmers/distances/kl_divergence.hpp"
 #include "optimize_cmp_kmers/get_trees.hpp"
+
+#include "optimize_cmp_kmers/cluster_container.hpp"
+#include "optimize_cmp_kmers/vlmc_template.hpp"
+
+// using matrix_t = Eigen::MatrixXd;
+
+using vlmc_c = container::VLMC_vector;
+using cluster_c = cluster::Cluster_vector<vlmc_c>;
 
 int main(int argc, char *argv[]){
   CLI::App app{"Distance comparison of either one directory or between two different directories."};
@@ -15,10 +25,24 @@ int main(int argc, char *argv[]){
   } catch (const CLI::ParseError &e) {
     return app.exit(e);
   }
-
   if(arguments.mode == parser::Mode::compare){
-    get_trees::get_trees(arguments.in_path);
+    if(arguments.in_path.empty()){
+      std::cerr
+          << "Error: A input path to .bintree files has to be given for comparison operation."
+          << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    if(arguments.to_path.empty()){
+      cluster_c trees{}; 
+      get_trees::get_trees<cluster_c, vlmc_c>(arguments.in_path, trees);
+    } else {
+      cluster_c left_trees{};
+      cluster_c right_trees{};
+      get_trees::get_trees<cluster_c, vlmc_c>(arguments.in_path, left_trees);
+      get_trees::get_trees<cluster_c, vlmc_c>(arguments.to_path, right_trees);
+    }
   }
-  
+
   return EXIT_SUCCESS;
 }
