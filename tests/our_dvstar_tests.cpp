5cb9eb5cdd5b1e2e9dc5aa256e0ab012550d63c8
JohanAtterforsStudent <atjohan@student.chalmers.se>
2023-02-10 09:02:15 +0100

    Making test work...

--- tests/our_dvstar_tests.cpp
+++ tests/our_dvstar_tests.cpp
@@ -11,6 +11,11 @@
 #include "distances/dvstar.hpp"
 #include "vlmc_from_kmers/dvstar.hpp"
 
+//Original implementation files
+#include <kmc_file.h>
+#include "vlmc_from_kmers/dvstar.hpp"
+#include "vlmc_from_kmers/build_vlmc.hpp"
+
 // using namespace dvstarnamespace;
 
 using vlmc_c = container::VLMC_vector;
@@ -26,15 +31,37 @@ protected:
   std::filesystem::path first_bintree{"../data/test_VLMCs/sequences_1.bintree"};
   std::filesystem::path second_bintree{"../data/test_VLMCs/sequences_2.bintree"};
   std::filesystem::path third_bintree{"../data/test_VLMCs/sequences_3.bintree"};
+
+  vlmc_c first_vlmc{first_bintree};
+  vlmc_c second_vlmc{second_bintree};
 };
 
 
 TEST_F(OurDvstarTests, Identity) {
-  vlmc_c first_vlmc{first_bintree};
+  std::filesystem::path tmp_path = std::filesystem::temp_directory_path();
+
+  std::filesystem::create_directories(tmp_path);
+  vlmc::configure_stxxl(tmp_path);
+
+  std::filesystem::path run_one_path{"out-one.bintree"};
+  int exit_one_code = vlmc::build_vlmc(first_fasta, 6, 2, 3.9075,
+                                       run_one_path, tmp_path, vlmc::Core::in);
+
+  std::filesystem::path run_two_path{"out-two.bintree"};
+  int exit_two_code = vlmc::build_vlmc(first_fasta, 6, 2, 3.9075,
+                                       run_two_path, tmp_path, vlmc::Core::in);
+
+  double dist_test = vlmc::dvstar(run_one_path, run_two_path, 0);
 
   double dist = distance::dvstar(first_vlmc, first_vlmc);
+  double dist_real = vlmc::dvstar(first_bintree, first_bintree, 0);
+  EXPECT_DOUBLE_EQ(dist_test, 0.0);
+}
 
-  EXPECT_FLOAT_EQ(dist, 0.0);
-  // double old_dist = vlmc::dvstar(first_bintree, first_bintree, 0);
-  // EXPECT_FLOAT_EQ(dist, old_dist);
+TEST_F(OurDvstarTests, EqualDistance) {
+
+  double dist_our = distance::dvstar(first_vlmc, second_vlmc);
+  double dist_real = vlmc::dvstar(first_bintree, second_bintree, 0);
+
+  EXPECT_DOUBLE_EQ(dist_our, dist_real);
 }
