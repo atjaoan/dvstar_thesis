79a4a45e37a225939e0884a809d75ea3a504551c
JohanAtterforsStudent <atjohan@student.chalmers.se>
2023-02-17 09:51:29 +0100

    Fix testsuit to new input kmer

--- tests/our_tests/vlmc_container_tests.cpp
+++ tests/our_tests/vlmc_container_tests.cpp
@@ -22,72 +22,54 @@ protected:
 };
 
 TEST_F(VlmcContainerTest, IndexRep1) {
-  container::VLMC_multi_vector container{};
+  container::Index_by_value container{};
   std::string kmer_string{"A"};
   auto created = create_kmer(kmer_string);
-  EXPECT_EQ(container.get_index_rep(created), 1);
+  container::RI_Kmer new_kmer{created};
+  EXPECT_EQ(new_kmer.integer_rep, 1);
 }
 
 TEST_F(VlmcContainerTest, IndexRep2) {
-  container::VLMC_multi_vector container{};
+  container::Index_by_value container{};
   std::string kmer_string{"T"};
   auto created = create_kmer(kmer_string);
-  EXPECT_EQ(container.get_index_rep(created), 4);
+  container::RI_Kmer new_kmer{created};
+  EXPECT_EQ(new_kmer.integer_rep, 4);
 }
 TEST_F(VlmcContainerTest, IndexRep3) {
-  container::VLMC_multi_vector container{};
+  container::Index_by_value container{};
   std::string kmer_string{"AA"};
   auto created = create_kmer(kmer_string);
-  EXPECT_EQ(container.get_index_rep(created), 5);
+  container::RI_Kmer new_kmer{created};
+  EXPECT_EQ(new_kmer.integer_rep, 5);
 }
 TEST_F(VlmcContainerTest, IndexRep4) {
-  container::VLMC_multi_vector container{};
+  container::Index_by_value container{};
   std::string kmer_string{"AT"};
   auto created = create_kmer(kmer_string);
-  EXPECT_EQ(container.get_index_rep(created), 8);
+  container::RI_Kmer new_kmer{created};
+  EXPECT_EQ(new_kmer.integer_rep, 8);
 }
 TEST_F(VlmcContainerTest, IndexRep5) {
-  container::VLMC_multi_vector container{};
+  container::Index_by_value container{};
   std::string kmer_string{"CC"};
   auto created = create_kmer(kmer_string);
-  EXPECT_EQ(container.get_index_rep(created), 10);
+  container::RI_Kmer new_kmer{created};
+  EXPECT_EQ(new_kmer.integer_rep, 10);
 }
 TEST_F(VlmcContainerTest, IndexRep6) {
-  container::VLMC_multi_vector container{};
+  container::Index_by_value container{};
   std::string kmer_string{"TG"};
   auto created = create_kmer(kmer_string);
-  EXPECT_EQ(container.get_index_rep(created), 19);
+  container::RI_Kmer new_kmer{created};
+  EXPECT_EQ(new_kmer.integer_rep, 19);
 }
 TEST_F(VlmcContainerTest, IndexRep7) {
-  container::VLMC_multi_vector container{};
+  container::Index_by_value container{};
   std::string kmer_string{"AAA"};
   auto created = create_kmer(kmer_string);
-  EXPECT_EQ(container.get_index_rep(created), 21);
-}
-
-TEST_F(VlmcContainerTest, AddToMultiVector) {
-  std::ifstream ifs(path_bintree, std::ios::binary);
-  cereal::BinaryInputArchive archive(ifs);
-  container::VLMC_multi_vector container{};
-  
-  Kmer kmer{};
-
-  if (ifs.peek() != EOF){
-    archive(kmer);
-    container.push(kmer);
-  }
-  ifs.close();
-  
-  int kmer_index = container.get_index_rep(kmer);
-  EXPECT_EQ(container.get(kmer_index).to_string(), kmer.to_string());
-}
-
-TEST_F(VlmcContainerTest, IndexAdd1) {
-  container::VLMC_multi_vector container{};
-  std::string kmer_string{"A"};
-  Kmer created = create_kmer(kmer_string);
-  container.push(created);
-  EXPECT_EQ(container.get(1).to_string(), "A");
+  container::RI_Kmer new_kmer{created};
+  EXPECT_EQ(new_kmer.integer_rep, 21);
 }
 
 TEST_F(VlmcContainerTest, AddReadInKmerToIndexByValue) {
@@ -113,7 +95,7 @@ TEST_F(VlmcContainerTest, AddManyReadInKmerToIndexByValue) {
     EXPECT_EQ(container.get(kmer.integer_rep), kmer);
   }
 }
-
+/*
 TEST_F(VlmcContainerTest, UseDirectoryIndexByValue) {
   container::Index_by_value container{path_bintree};
   container::VLMC_multi_vector multi_vec{path_bintree};
@@ -122,3 +104,4 @@ TEST_F(VlmcContainerTest, UseDirectoryIndexByValue) {
     EXPECT_EQ(container.get(i).integer_rep, i);
   }
 }
+*/
