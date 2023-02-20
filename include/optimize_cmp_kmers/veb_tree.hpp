682af2739cf088e86773c768c00c9f2c78c6bf2d
Johan <atjohan@student.chalmers.se>
2023-02-20 17:10:30 +0100

    Add Kmers, fix strange int->kmer cast, does not pass tests

--- include/optimize_cmp_kmers/veb_tree.hpp
+++ include/optimize_cmp_kmers/veb_tree.hpp
@@ -18,6 +18,7 @@ namespace veb{
 struct Veb_tree{
   container::RI_Kmer min;
   container::RI_Kmer max;
+  container::RI_Kmer null_kmer = container::RI_Kmer(-1);
   size_t size = 0;
   bool is_empty;
   std::unique_ptr<Veb_tree> summary;
@@ -42,9 +43,11 @@ struct Veb_tree{
 
   size_t tree_group(size_t in){ return (in / size); }
   size_t tree_group_index(size_t in){ return (in % size); }
+  //container::RI_Kmer tree_group(container::RI_Kmer in){ return (in / size); }
+  //container::RI_Kmer tree_group_index(container::RI_Kmer in){ return (in % size); }
 };
 
-void insert(Veb_tree &t, container::RI_Kmer in) {
+void insert(Veb_tree &t, container::RI_Kmer& in) {
   if(t.is_empty){
     t.min = in;
     t.max = in;
@@ -64,12 +67,14 @@ void insert(Veb_tree &t, container::RI_Kmer in) {
   size_t c = t.tree_group(in.integer_rep);
   size_t i = t.tree_group_index(in.integer_rep);
   if(t.trees[c] == nullptr){
-    insert(*t.summary, c);
+    in.integer_rep = c;
+    insert(*t.summary, in);
   }
 
   if(t.trees[c] == nullptr)
     t.trees[c] = std::make_shared<Veb_tree>(t.size);
-  insert(*t.trees[c], i);
+  in.integer_rep = i;
+  insert(*t.trees[c], in);
   return;
   }
 
@@ -78,5 +83,32 @@ container::RI_Kmer& find(Veb_tree &t, container::RI_Kmer in) {
   if(t.max == in) return t.max;
   size_t c = t.tree_group(in.integer_rep);
   find(*t.trees[c], in);
+}
+
+container::RI_Kmer succ(Veb_tree&t, container::RI_Kmer in){
+  std::cout << "1" << std::endl;
+  if(t.is_empty) return t.null_kmer;
+  std::cout << "2" << std::endl;
+  if(in < t.min) return t.min;
+  std::cout << "3" << std::endl;
+
+  size_t c = t.tree_group(in.integer_rep);
+  size_t i = t.tree_group_index(in.integer_rep);
+  auto temp = container::RI_Kmer(i);
+  std::cout << "before if" << std::endl; 
+  if(t.trees[c] != nullptr && in < t.trees[c]->max){
+    std::cout << "first if" << std::endl;
+    return succ(*t.trees[c], temp); //c*std::ceil(std::sqrt(t.size)) + 
+  } else{
+    std::cout << "else" << std::endl;
+    int c_prim = succ(*t.summary, c).integer_rep;
+    if(c_prim == -1) {
+      std::cout << "c_prim == -1" << std::endl; 
+      return t.max;
+    } else {
+      std::cout << "last" << std::endl; 
+      return t.trees[c_prim]->min; //c_prim*std::ceil(std::sqrt(t.size)) + 
+    }
+  }
 }
 }
--- tests/our_tests/veb_tree_tests.cpp
+++ tests/our_tests/veb_tree_tests.cpp
@@ -32,52 +32,61 @@ TEST_F(VebTreeTest, SizedConstructor) {
 }
 TEST_F(VebTreeTest, InsertOne) {
   veb::Veb_tree tree{16};
-  veb::insert(tree, container::RI_Kmer{1});
-  EXPECT_EQ(tree.min, container::RI_Kmer{1});
-  EXPECT_EQ(tree.max, container::RI_Kmer{1});
+  auto kmer =  container::RI_Kmer{1};
+  veb::insert(tree, kmer);
+  EXPECT_EQ(tree.min, kmer);
+  EXPECT_EQ(tree.max, kmer);
   EXPECT_EQ(tree.size, 16);
   EXPECT_EQ(nullptr, tree.trees[0]);
   EXPECT_NE(nullptr, tree.summary);
 }
 TEST_F(VebTreeTest, SummaryTreeAfterTwoInsert) {
   veb::Veb_tree tree{16};
-  veb::insert(tree, container::RI_Kmer{1});
-  veb::insert(tree, container::RI_Kmer{2});
+  auto kmer1 = container::RI_Kmer{1};
+  auto kmer2 = container::RI_Kmer{2};
+  veb::insert(tree, kmer1);
+  veb::insert(tree, kmer2);
   EXPECT_NE(nullptr, tree.summary);
   EXPECT_EQ(tree.summary->size, 4);
-  EXPECT_EQ(tree.summary->max, container::RI_Kmer{0});
-  EXPECT_EQ(tree.summary->min, container::RI_Kmer{0});
+  EXPECT_EQ(tree.summary->max, kmer2);
+  EXPECT_EQ(tree.summary->min, kmer2);
 }
 
 TEST_F(VebTreeTest, SubTreeAfterTwoInsert) {
   veb::Veb_tree tree{16};
-  veb::insert(tree, 1);
-  veb::insert(tree, 0);
+  auto kmer1 = container::RI_Kmer{1};
+  auto kmer0 = container::RI_Kmer{0};
+  veb::insert(tree, kmer1);
+  veb::insert(tree, kmer0);
   EXPECT_NE(nullptr, tree.summary);
-  EXPECT_EQ(container::RI_Kmer{0}, tree.summary->min);
-  EXPECT_EQ(container::RI_Kmer{0}, tree.summary->max);
-  EXPECT_EQ(tree.trees[0]->min, container::RI_Kmer{1});
-  EXPECT_EQ(tree.trees[0]->max, container::RI_Kmer{1});
-  EXPECT_EQ(tree.min, 0);
+  EXPECT_EQ(kmer0, tree.summary->min);
+  EXPECT_EQ(kmer0, tree.summary->max);
+  EXPECT_EQ(tree.trees[0]->min, kmer1);
+  EXPECT_EQ(tree.trees[0]->max, kmer1);
+  EXPECT_EQ(tree.min, kmer0);
 }
 
 TEST_F(VebTreeTest, FindOnOneInsert) {
   veb::Veb_tree tree{16};
-  veb::insert(tree, container::RI_Kmer{1});
-  EXPECT_EQ(container::RI_Kmer{1}, veb::find(tree, container::RI_Kmer{1}));
+  auto kmer1 = container::RI_Kmer{1};
+  veb::insert(tree, kmer1);
+  EXPECT_EQ(kmer1, veb::find(tree, kmer1));
 }
 
 TEST_F(VebTreeTest, FindOnTwoInsert) {
   veb::Veb_tree tree{16};
-  veb::insert(tree, container::RI_Kmer{1});
-  veb::insert(tree, container::RI_Kmer{0});
-  EXPECT_EQ(container::RI_Kmer{1}, veb::find(tree, container::RI_Kmer{1}));
-  EXPECT_EQ(container::RI_Kmer{0}, veb::find(tree, container::RI_Kmer{0}));
+  auto kmer1 = container::RI_Kmer{1};
+  auto kmer0 = container::RI_Kmer{0};
+  veb::insert(tree, kmer1);
+  veb::insert(tree, kmer0);
+  EXPECT_EQ(kmer1, veb::find(tree, kmer1));
+  EXPECT_EQ(kmer0, veb::find(tree, kmer0));
 }
-
+/*
 TEST_F(VebTreeTest, Insert16Find16) {
   std::vector<container::RI_Kmer> kmers {};
   for (size_t i = 0; i < 16; i++){
+    auto kmer1 = container::RI_Kmer{i};
     kmers.push_back(container::RI_Kmer{i});
   }
   
@@ -90,3 +99,13 @@ TEST_F(VebTreeTest, Insert16Find16) {
     EXPECT_EQ(kmers[i], veb::find(tree, kmers[i]));
   }
 }
+
+TEST_F(VebTreeTest, FindOneSucc) {
+  veb::Veb_tree tree{16};
+  auto kmer1 = container::RI_Kmer{1};
+  auto kmer2 = container::RI_Kmer{2};
+  veb::insert(tree, kmer1);
+  veb::insert(tree, kmer2);
+  EXPECT_EQ(kmer2, veb::succ(tree, kmer1));
+}
+*/
