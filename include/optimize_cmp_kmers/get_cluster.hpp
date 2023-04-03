f53255b234a7275aa30e25ef621887dab15ce8b3
Sebastian <holmse@chalmers.se>
2023-04-03 17:19:44 +0200

    Added parallelization for creation of container for kmer-major implementation. Also removed prints from vlmc container and fixed some tests that were checking identity for old dvstar implementation.

--- include/optimize_cmp_kmers/get_cluster.hpp
+++ include/optimize_cmp_kmers/get_cluster.hpp
@@ -11,6 +11,7 @@
 #include <limits.h>
 #include <exception>
 #include <algorithm>
+#include <mutex>
 
 #include "cluster_container.hpp"
 #include "vlmc_container.hpp"
@@ -64,27 +65,52 @@ container::Cluster_Container<VC> old_get_cluster(const std::filesystem::path &di
 
 // Return as reference?
 container::Kmer_Cluster get_kmer_cluster(const std::filesystem::path &directory, const size_t background_order = 0){
-  container::Kmer_Cluster cluster{};
-  size_t id = 0; 
+  std::vector<std::filesystem::path> paths{};
+
+  auto nr_cores_to_use = 4; 
+  auto set_size = -1; 
+
+  if(nr_cores_to_use > 4) nr_cores_to_use = 4;
   for (const auto& dir_entry : recursive_directory_iterator(directory)) {
-    std::vector<container::RI_Kmer> input_vector{};  
-    Eigen::ArrayX4f cached_context((int)std::pow(4, background_order), 4);
-
-    auto fun = [&](const RI_Kmer &kmer) { input_vector.push_back(kmer); }; 
-
-    int offset_to_remove = container::load_VLMCs_from_file(dir_entry.path(), cached_context, fun, background_order); 
-
-    for (RI_Kmer kmer : input_vector){
-      int background_idx = kmer.background_order_index(kmer.integer_rep, background_order);
-      int offset = background_idx - offset_to_remove;
-      kmer.next_char_prob *= cached_context.row(offset).rsqrt();
-      cluster.push(container::Kmer_Pair{kmer, id}); 
-    }
-    id++; 
+    paths.push_back(dir_entry.path());
   }
 
-  cluster.set_size(id); 
+  size_t paths_size = paths.size();
+  if ((set_size != -1) && (set_size < paths_size)){
+    paths_size = set_size; 
+  }
 
-  return cluster; 
+  container::Kmer_Cluster final_cluster{}; 
+
+  std::mutex cluster_lock;
+
+  auto fun = [&](size_t start_index, size_t stop_index) {
+    container::Kmer_Cluster cluster{}; 
+    for (int index = start_index; index < stop_index; index++){
+      std::vector<container::RI_Kmer> input_vector{};  
+      Eigen::ArrayX4f cached_context((int)std::pow(4, background_order), 4);
+
+      auto fun = [&](const RI_Kmer &kmer) { input_vector.push_back(kmer); }; 
+
+      int offset_to_remove = container::load_VLMCs_from_file(paths[index], cached_context, fun, background_order); 
+
+      for (RI_Kmer kmer : input_vector){
+        int background_idx = kmer.background_order_index(kmer.integer_rep, background_order);
+        int offset = background_idx - offset_to_remove;
+        kmer.next_char_prob *= cached_context.row(offset).rsqrt();
+        cluster.push(container::Kmer_Pair{kmer, index}); 
+      }
+    } 
+
+    std::lock_guard<std::mutex> lock(cluster_lock);
+    final_cluster.push_all(cluster); 
+  };
+
+  parallel::parallelize(paths_size, fun, nr_cores_to_use);
+
+  final_cluster.set_size(paths_size); 
+
+
+  return final_cluster; 
 }
 }
