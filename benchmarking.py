beb329ff71772f597e198706ba548ffa3e2a9cc3
Sebastian <holmse@chalmers.se>
2023-04-28 00:44:24 +0200

    Fixed parallelization benchmarking

--- benchmarking.py
+++ benchmarking.py
@@ -222,7 +222,7 @@ def Pst_normal_benchmaking(dataset: str):
 # cache-misses on dataset with new   #
 # implementation.                    #
 ######################################
-def normal_benchmaking(dataset: str, implementation: str, dataset_outer_dir: str):
+def normal_benchmaking(dataset: str, implementation: str):
     csv_filename = get_csv_name(dataset, implementation + "_")
     print(csv_filename)
     nb_files = count_nb_files(cwd / dataset / "small")
@@ -270,37 +270,49 @@ def normal_benchmaking(dataset: str, implementation: str, dataset_outer_dir: str
 # Function for benchmarking degree  #
 # of parallelization on dataset.    #  
 #####################################
-def parallelization_benchmark(dataset: str, implementation: str, dataset_outer_dir: str):
+def parallelization_benchmark(dataset: str, implementation: str):
     csv_filename = get_csv_name(dataset, "parallelization_")
     print(csv_filename)
+
+    th_small, min_small, max_small = get_parameter_from_bintree(os.listdir(cwd / dataset / "small")[0])
+    th_medium, min_medium, max_medium = get_parameter_from_bintree(os.listdir(cwd / dataset / "medium")[0])
+    th_large, min_large, max_large = get_parameter_from_bintree(os.listdir(cwd / dataset / "large")[0])
+
     nb_files = count_nb_files(cwd / dataset / "small")
 
-    nb_files = int(nb_files / 2)
+    files_run = 2
+    while (files_run * 2 < nb_files):
+        files_run = files_run * 2
+
     nr_cores = 1
 
-    #th_small, min_small, max_small = get_parameter_from_bintree(os.listdir(cwd / dataset / "small")[0])
-    #th_medium, min_medium, max_medium = get_parameter_from_bintree(os.listdir(cwd / dataset / "medium")[0])
-    th_large, min_large, max_large = get_parameter_from_bintree(os.listdir(cwd / dataset / "large")[0])
-    while (nr_cores <= 16):
-        #print("Benchmarking small with " + str(nr_cores) + " cores.")
-        #res_our_small  = calculate_distance(nb_files, dataset + "/small", "hdf5_results/distances_parallel_small.hdf5", implementation, nr_cores, 0)
-        #print("Benchmarking medium with " + str(nr_cores) + " cores.")
-        #res_our_medium = calculate_distance(nb_files, dataset + "/medium", "hdf5_results/distances_parallel_medium.hdf5", implementation, nr_cores, 0)
-        print("Benchmarking large with " + str(nr_cores) + " cores.")
-        res_our_large  = calculate_distance(nb_files, dataset + "/large", "hdf5_results/distances_parallel_large.hdf5", implementation, nr_cores, 0)
-
-        #compare_hdf5_files("_parallel", "_small", implementation, dataset_outer_dir + "_small", nr_cores, nb_files, 0)
-        #compare_hdf5_files("_parallel", "_medium", implementation, dataset_outer_dir + "_medium", nr_cores, nb_files, 0)
-        #compare_hdf5_files("_parallel", "_large", implementation, dataset_outer_dir + "_large", nr_cores, nb_files, 0)
-
-        #catch_and_save(res_our_small, cwd / csv_filename, "small", nb_files, th_small, min_small, max_small, implementation, nr_cores)
-        #catch_and_save(res_our_medium, cwd / csv_filename, "medium", nb_files, th_medium, min_medium, max_medium, implementation, nr_cores)
-        catch_and_save(res_our_large, cwd / csv_filename, "large", nb_files, th_large, min_large, max_large, implementation, nr_cores)
-        nr_cores = nr_cores * 2
+    if (os.path.isfile(cwd / "hdf5_results/Dvstar/distances_small.hdf5")):
+        os.remove(cwd / "hdf5_results/Dvstar/distances_small.hdf5")
+    if (os.path.isfile(cwd / "hdf5_results/Dvstar/distances_medium.hdf5")):
+        os.remove(cwd / "hdf5_results/Dvstar/distances_medium.hdf5")
+    if (os.path.isfile(cwd / "hdf5_results/Dvstar/distances_large.hdf5")):
+        os.remove(cwd / "hdf5_results/Dvstar/distances_large.hdf5")
     
-    #os.remove(cwd / "hdf5_results/distances_small.hdf5")
-    #os.remove(cwd / "hdf5_results/distances_medium.hdf5")
-    #os.remove(cwd / "hdf5_results/distances_large.hdf5")
+    while (nr_cores <= 16):
+        print("Benchmarking small with " + str(nr_cores) + " cores.")
+        res_our_small  = calculate_distance(files_run, dataset + "/small", "hdf5_results/Dvstar/distances_small.hdf5", implementation, nr_cores, 0)
+        print("Benchmarking medium with " + str(nr_cores) + " cores.")
+        res_our_medium = calculate_distance(files_run, dataset + "/medium", "hdf5_results/Dvstar/distances_medium.hdf5", implementation, nr_cores, 0)
+        print("Benchmarking large with " + str(nr_cores) + " cores.")
+        res_our_large  = calculate_distance(files_run, dataset + "/large", "hdf5_results/Dvstar/distances_large.hdf5", implementation, nr_cores, 0)
+
+        compare_hdf5_files(dataset, "small", str(files_run))
+        compare_hdf5_files(dataset, "medium", str(files_run))
+        compare_hdf5_files(dataset, "large", str(files_run))
+
+        catch_and_save(res_our_small, cwd / csv_filename, "small", nb_files, th_small, min_small, max_small, implementation, nr_cores)
+        catch_and_save(res_our_medium, cwd / csv_filename, "medium", nb_files, th_medium, min_medium, max_medium, implementation, nr_cores)
+        catch_and_save(res_our_large, cwd / csv_filename, "large", nb_files, th_large, min_large, max_large, implementation, nr_cores)
+        
+        nr_cores = nr_cores * 2
+        os.remove(cwd / "hdf5_results/Dvstar/distances_small.hdf5")
+        os.remove(cwd / "hdf5_results/Dvstar/distances_medium.hdf5")
+        os.remove(cwd / "hdf5_results/Dvstar/distances_large.hdf5")
 
 def compare_hdf5_files(dataset: str, size: str, files_run: str):
     vlmc_file = h5py.File(cwd / "hdf5_results" / "Dvstar" / ("distances_" + size + ".hdf5"), 'r')
@@ -326,18 +338,19 @@ def compare_hdf5_files(dataset: str, size: str, files_run: str):
 
 @app.command()
 def benchmark():
-    # compare_hdf5_files("data/benchmarking/human", "small", "2")
     # ECOLI BENCHMARKING 
     # Pst_normal_benchmaking("data/benchmarking/ecoli")
-    # normal_benchmaking("data/benchmarking/ecoli", "sorted-vector", "ecoli")
-    # normal_benchmaking("data/benchmarking/ecoli", "hashmap", "ecoli")
-    # normal_benchmaking("data/benchmarking/ecoli", "combo", "ecoli")
+    # normal_benchmaking("data/benchmarking/ecoli", "sorted-vector")
+    # normal_benchmaking("data/benchmarking/ecoli", "hashmap")
+    # normal_benchmaking("data/benchmarking/ecoli", "combo")
 
     # HUMAN BENCHMARKING
     # Pst_normal_benchmaking("data/benchmarking/human")
-    normal_benchmaking("data/benchmarking/human", "sorted-vector", "human")
-    # normal_benchmaking("data/benchmarking/human", "hashmap", "human")
-    # normal_benchmaking("data/benchmarking/human", "combo", "human")
+    # normal_benchmaking("data/benchmarking/human", "sorted-vector")
+    # normal_benchmaking("data/benchmarking/human", "hashmap")
+    # normal_benchmaking("data/benchmarking/human", "combo")
+
+    parallelization_benchmark("data/benchmarking/human", "sorted-vector")
 
 if __name__ == "__main__":
     app()
